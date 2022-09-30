07d40e190d5f74a1ac885dcc0d9eb2434d6fbfe3
cbarrios <lalobarrios16@yahoo.com>
2022-09-30 17:35:18 -0400

    master: (3) go to activity when clicking

--- app/src/main/java/com/example/notifications/MainActivity.kt
+++ app/src/main/java/com/example/notifications/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.notifications
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -41,6 +43,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun createSimpleNotification() {
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val flag =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else 0
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, flag)
+
         val builder = NotificationCompat.Builder(this, MY_CHANNEL_ID)
             .setSmallIcon(android.R.drawable.ic_delete)
             .setContentTitle("My title")
@@ -50,6 +59,8 @@ class MainActivity : AppCompatActivity() {
                 NotificationCompat.BigTextStyle()
                     .bigText("Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello")
             )
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
         NotificationManagerCompat.from(this).notify(1, builder.build())
     }
 }
